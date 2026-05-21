40ed705bc3b63348376310bd4fd98e6e10a6b8f3
ManuelNeto280690 <manuel.neto280690@gmail.com>
2026-05-21 03:30:13 +0100

    Altera rota principal para retornar JSON da API

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'api' => 'Ceftic API',
+        'version' => '1.0.0',
+        'status' => 'online',
+    ]);
 });
