818366478b01004eee2f0f8051edfb253b21e876
ManuelNeto280690 <manuel.neto280690@gmail.com>
2026-05-21 03:31:00 +0100

    Corrige nome da API para AcademTI

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return response()->json([
-        'api' => 'Ceftic API',
+        'api' => 'AcademTI API',
         'version' => '1.0.0',
         'status' => 'online',
     ]);
